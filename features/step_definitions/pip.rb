6981a7bb5d83628d9f6a3a442ceefa1af8a5d63c
Sthitapragyan Mohanty <M1041925@mindtree.com>
2019-01-18 14:23:33 +0530

    small changes

--- features/step_definitions/pip.rb
+++ features/step_definitions/pip.rb
@@ -1,16 +1,13 @@
 When("I go for the PIP options") do
-  sleep 4
+  sleep 1
   page.find('div#smphtml5iframempwrp').hover
   within_frame 'smphtml5iframemp' do
     page.first('.p_button.p_controlBarButton.p_picInPicButton').click
-    sleep 4
   end
 end
 
 When("I find the pause button in the PIP window") do
-  sleep 3
   within_frame 'smphtml5iframemp' do
     page.find('button[title="Exit Picture in Picture mode"]')
-    sleep 4
   end
 end
