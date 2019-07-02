40122bb789e4f1ae833eee977e4e20d0f2786891
Bevis Owusu-Anning <bevis.owusu-anning@bbc.co.uk>
2019-07-02 14:23:58 +0100

    update to quality setting to remove code

--- features/step_definitions/page_interactions.rb
+++ features/step_definitions/page_interactions.rb
@@ -28,15 +28,9 @@ Then("I verify low option has been selected") do
         sleep(1)
         page.first("#p_qualityTitle").click
 
-      if page.driver.browser.browser == :chrome
-        # Chrome needs an additional press for some reason
-        sleep(1)
-        page.first(".p_subsToggle").click
-        sleep(1)
-        page.first(".p_subtitleButton").click
       end
 
-        sleep(1)
+      sleep(1)
       page.first("#p_button.p_controlBarButton.p_playbackSettingsButton.p_dontCloseSettingsPanel_p_qualityTitle_p_settingsSubtitle").click
       sleep(1)
       expect(page.find('button#p_button.p_controlBarButton.p_playbackSettingsButton.p_dontCloseSettingsPanel_p_qualityTitle_p_settingsSubtitle')['p_settingsSubtitle']).to eq("Low Quality")
