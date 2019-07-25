4ebbcc41ab7cc6fea9f9765ed38fdd0c3d5bca65
Bevis Owusu-Anning <bevis.owusu-anning@bbc.co.uk>
2019-07-25 16:09:28 +0100

    First commit to get started

--- features/step_definitions/page_interactions.rb
+++ features/step_definitions/page_interactions.rb
@@ -9,4 +9,22 @@ Then(/^I refresh page$/) do
   sleep (3)
 end
 
+Then(/^I click guidance$/) do
+  page.refresh()
+  sleep (3)
+end
 
+When(/^I hover and click$/) do
+  within_frame 'smphtml5iframemp' do
+    page.first("div.p_guidanceContainer").hover
+    sleep(2)
+    # focus thr guidance message and click 
+    if page.has_css?('#p_ctaGuidance p_guidance') == true
+      find('#p_ctaGuidance p_guidance').click
+    end
+  end
+end
+
+
+
+yey. d
