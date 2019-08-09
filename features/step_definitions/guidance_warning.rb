e6d97e4c5c06be70f70f2a2b3afbff62a729242c
Bevis Owusu-Anning <bevis.owusu-anning@bbc.co.uk>
2019-08-09 10:44:00 +0100

    added scenario outline and audi-vid

--- features/step_definitions/guidance_warning.rb
+++ features/step_definitions/guidance_warning.rb
@@ -5,17 +5,12 @@ Then(/^I can see warning$/) do
   end
 end
 
-Given(/^I am on a page with warning installed$/) do
-  visit('https://is.gd/hexoxi')
-end
-
 Then(/^I verify guidance text$/) do
       within_frame 'smphtml5iframemp' do
 	  find('.p_guidanceText').should have_content('This is a WARNING for Audio content')
 	  sleep(2)
   end
 end
-
 Then(/^I verify holding img guidance text$/) do
       within_frame 'smphtml5iframemp' do
     find('.p_guidanceText').should have_content('BEVIS WARNING TEST')
@@ -31,13 +26,6 @@ Then(/^I can see in player warning$/) do
   end
 end
 
-Then(/^I click guidance$/) do
-  page.refresh()
-  sleep (3)
-end
-
-#Guidance Minimode 
-
 Given(/^I am on a minimode audio page with old UI and guidance$/) do
   visit('https://is.gd/inarix')
 end
@@ -49,15 +37,26 @@ Then(/^I can see audio guidance$/) do
   end
 end
 
-When(/^I see correct guidance message$/) do
+When(/^I see correct queued playlist guidance "([^"]*)"$/) do |arg1|
   within_frame 'smphtml5iframemp' do
-    sleep(2)
-    find('.p_guidanceText').should have_content('Guidance Test')
-    sleep(2)
+    page.first("div.p_accessibleHitArea").hover
+    if arg1 == "inplay_eng"
+      sleep(2)
+      find('.p_guidanceText').should have_content('This is a WARNING for Audio content')
+      sleep(2)
+    elsif arg1 == "inplay_grk" 
+      sleep(2)
+      find('.p_guidanceText').should have_content('Πρόκειται για μια ΠΡΟΕΙΔΟΠΟΙΗΣΗ για το περιεχόμενο ήχου')
+      sleep(2)
+    elsif arg1 == "inplay_jap" 
+      sleep(2)
+      find('.p_guidanceText').should have_content('これは音声コンテンツの警告です')
+      sleep(2)
+    end
   end
 end
 
-When(/^I see correct guidance message in "([^"]*)"$/) do |arg1|
+When(/^I see correct guidance or warning message "([^"]*)"$/) do |arg1|
   within_frame 'smphtml5iframemp' do
     page.first("div.p_accessibleHitArea").hover
 	if arg1 == "english"
@@ -72,29 +71,33 @@ When(/^I see correct guidance message in "([^"]*)"$/) do |arg1|
 	  sleep(2)
 	  find('.p_guidanceText').should have_content('Δοκιμασία καθοδήγησης')
 	  sleep(2)
+  elsif arg1 == "warn_eng" 
+    sleep(2)
+    find('.p_guidanceText').should have_content('BEVIS WARNING TEST')
+    sleep(2)
+  elsif arg1 == "warn_grk" 
+    sleep(2)
+    find('.p_guidanceText').should have_content('ΔΟΚΙΜΗ ΠΡΟΕΙΔΟΠΟΙΗΣΗΣ ΠΡΟΣΟΧΗΣ')
+    sleep(2)
+  elsif arg1 == "warn_jap" 
+    sleep(2)
+    find('.p_guidanceText').should have_content('証明警告テスト')
+    sleep(2)
 	end
   end
 end
-
-#Guidance Simulcast
 
 Given(/^I am on a Simulcast page with guidance$/) do
   visit('https://is.gd/erimih')
 end
 
-#Old audio player 
-
 Given(/^I am on a old audio player page with guidance$/) do
   visit('https://is.gd/ujahus')
 end
 
-#News page with greek guidance
-
 Given(/^I am on a news page with greek guidance$/) do
   visit('https://is.gd/otujul')
 end
-
-#News page with japanese guidance
 
 Given(/^I am on a news page with japanese guidance$/) do
   visit('https://is.gd/yipiso')
